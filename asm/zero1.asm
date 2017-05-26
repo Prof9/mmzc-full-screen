0d6a46b801c5acf0a9eae0e45f7e48c6eff7caba
Prof. 9 <prof9@outlook.com>
2017-05-27 00:48:02 +0200

    Zero1: Add camera pushback in all 4 directions.

--- asm/zero1.asm
+++ asm/zero1.asm
@@ -4,115 +4,168 @@
 //----------------------------------
 // Camera push-back at edge of map.
 //----------------------------------
+Z1_CameraPushBack:
 .thumb
 .org 0x02065564
-.area 0xC8	// holy geez this is a tight fit
+.area 0xC8	// holy geez this is a tight fit...
+@@main:
 	push	r0-r1,r4-r7,r14
 
-	add	r6,=@@offsets
+	mov	r2,9*4
+	add	r6,=@@values
+
+	bl	@@checkRoom
+	add	r7,r3,1h		// r7 = base room
 
 	ldmia	[r0]!,r0,r5		// r0 = x, r5 = y
-	ldrh	r1,[r6,@@vals - @@offsets + 0]
+	mov	r1,120
+	lsl	r1,r1,8h
 	blx	2023ED4h
 	mov	r4,r1			// r4 = x % 120.0
 	mov	r0,r5			// r0 = y
-	ldrh	r1,[r6,@@vals - @@offsets + 2]
+	mov	r1,80
+	lsl	r1,r1,8h
 	blx	2023ED4h
-	mov	r5,r1			// r5 = x %  80.0
+	mov	r2,r1			// r2 = y %  80.0
 	pop	r0-r1
 
-	bl	@@checkRoom		// get base room
-	add	r7,r2,1h		// r7 = base room
+	mov	r3,2-1
+@@main_loop:
+	mov	r5,0h
+	bl	@@direction
+	bl	@@direction
+	stmia	[r1]!,r5
+	mov	r4,r2
+	sub	r3,1h
+	bpl	@@main_loop
 
-@@checkH:
-	mov	r3,0h			// init camera x offset
-@@checkLeft:
-	bl	@@checkRoom		// 2nd call
-	beq	@@checkRight
-	ldrh	r2,[r6,@@vals - @@offsets + 0 - 2*2]
-	sub	r3,r2,r4		// r3 += 120.0 - x % 120
-@@checkRight:
-	bl	@@checkRoom		// 3rd call
-	beq	@@checkV
-	sub	r3,r3,r4		// r3 -= x % 120.0
-
-@@checkV:
-	mov	r4,0h			// init camera y offset
-@@checkUp:
-	bl	@@checkRoom		// 4th call
-	beq	@@checkDown
-	ldrh	r2,[r6,@@vals - @@offsets + 2 - 2*4]
-	sub	r4,r2,r5		// r4 += 80 - y % 80
-@@checkDown:
-	bl	@@checkRoom		// 5th call
-	beq	@@checkExtra
-	sub	r4,r4,r5		// r4 -= y % 80
-@@checkExtra:
-	bl	@@checkRoom		// 6th call
-	bne	@@end
-@@checkExtra1:
-	bl	@@checkRoom		// 7th call
-	beq	@@checkExtra2
-	
-	mov	r2,16
-	lsl	r2,r2,8h
-	sub	r4,r4,r2		// r4 -= 16
-	b	@@end
-@@checkExtra2:
-	bl	@@checkRoom		// 8th call
-	beq	@@end
-	mov	r2,64
-	lsl	r2,r2,8h
-	sub	r2,r5,r2
-	sub	r4,r4,r2		// r4 -= y % 80 - 64
-
-@@end:
-	stmia	[r1]!,r3,r4
 	pop	r4-r7,r15
+
+.align 4
+@@values:
+	// push camera right
+	.db	-120 >> 3,    0 >> 3			// room to   left
+	.db	 256 >> 3,    0 >> 3			// room to  right
+	.db	-128 >> 3,    0 >> 3			// room to   left
+	.db	 120 >> 3,    8 >> 3,    8 >> 3		// LHS
+	// push camera left
+	.db	 120 >> 3,    0 >> 3			// room to  right
+	.db	-256 >> 3,    0 >> 3			// room to   left
+	.db	 128 >> 3,    0 >> 3			// room to  right
+	.db	   0 >> 3,   -8 >> 3,  112 >> 3		// LHS
+	// push camera down
+	.db	   0 >> 3,  -80 >> 3			// room to    top
+	.db	   0 >> 3,  192 >> 3			// room to bottom
+	.db	   0 >> 3,  -96 >> 3			// room to    top
+	.db	  80 >> 3,   16 >> 3,   16 >> 3		// LHS
+	// push camera up
+	.db	   0 >> 3,   80 >> 3			// room to bottom
+	.db	   0 >> 3, -192 >> 3			// room to    top
+	.db	   0 >> 3,   96 >> 3			// room to bottom
+	.db	   0 >> 3,  -16 >> 3,   64 >> 3		// LHS
+	// get base room
+	.db	   0 >> 0,    0 >> 3			// current room
+
+@@direction:
+	// in:
+	//	r0 = ptr to camera base position
+	//	r4 = RHS
+	//	r5 = camera position modifier
+	//	r6 = values ptr
+	//	r7 = base room
+	// out:
+	//	r5 = camera position modifier
+	//	r6 = incremented offsets
+	push	r2-r3,r14
+
+@@direction_standard:
+	mov	r2,0h
+	bl	@@checkRoom
+	beq	@@direction_extra
+	mov	r2,6h
+	ldsb	r2,[r6,r2]
+	lsl	r2,r2,3+8
+	sub	r2,r2,r4
+	add	r5,r5,r2
+
+@@direction_extra:
+	mov	r2,2h
+	bl	@@checkRoom
+	bne	@@direction_end
+@@direction_full:
+	// r2 = 0 when CheckRoom returns false
+	bl	@@checkRoom
+	beq	@@direction_partial
+	mov	r2,7h
+	ldsb	r2,[r6,r2]
+	lsl	r2,r2,3+8
+	add	r5,r5,r2
+	b	@@direction_end
+@@direction_partial:
+	mov	r2,4h
+	bl	@@checkRoom
+	beq	@@direction_end
+	mov	r2,8h
+	ldsb	r2,[r6,r2]
+	lsl	r2,r2,3+8
+	sub	r2,r2,r4
+	add	r5,r5,r2
+
+@@direction_end:
+	add	r6,9h
+	pop	r2-r3,r15
 
 @@checkRoom:
 	// in:
-	//	r0 = zeroPos
-	//	r6 = offsets
+	//	r0 = ptr to camera base position
+	//	r2 = values offset
+	//	r6 = values base ptr
 	//	r7 = base room
 	// out:
-	//	
-	//	r2 = room number
-	//	r6 = offsets
-	push	r0-r1,r3-r5,r7,r14
-	ldmia	[r0]!,r0,r1		// r0 = player.x, r1 = player.y
-	mov	r2,0h
+	//	z  = same room as base room
+	//	r2 = 0 if not same room as base room
+	//	r3 = room number
+	push	r0-r1,r14
+	ldmia	[r0]!,r0,r1		// r0 = base x, r1 = base y
+	add	r3,r2,1h
 	ldsb	r2,[r6,r2]
-	mov	r3,1h
 	ldsb	r3,[r6,r3]
-	add	r6,2h			// increment offsets
 	lsl	r2,r2,3+8		// r2 = x offset
 	lsl	r3,r3,3+8		// r3 = y offset
 	add	r0,r0,r2
 	add	r1,r1,r3
 	bl	2065A2Ch
-	mov	r2,r0
+	mov	r3,r0
 	sub	r0,r7,r0
 	bmi	@@checkRoom_end
 	cmp	r0,2h
 	bgt	@@checkRoom_end
-	mov	r0,0h
+	mov	r2,0h
 @@checkRoom_end:
-	pop	r0-r1,r3-r5,r7,r15
+	pop	r0-r1,r15
+.endarea	// ...but we made it work somehow
 
-.align 4
-@@offsets:
-	.db	   0 >> 3,    0 >> 3	// 1st call
-	.db	-120 >> 3,    0 >> 3	// 2nd call
-	.db	 120 >> 3,    0 >> 3	// 3rd call
-	.db	   0 >> 3,  -80 >> 3	// 4th call
-	.db	   0 >> 3,   80 >> 3	// 5th call
-	.db	   0 >> 3, -192 >> 3	// 6th call
-	.db	   0 >> 3,   80 >> 3	// 7th call
-	.db	   0 >> 3,   96 >> 3	// 8th call
-@@vals:
-	.dh	 120 << 8,   80 << 8
-.endarea
+// Some world block/room number changes to fix some camera scrolling issues.
+// Resistance Base, elevator left to room with door leading to Trans Server
+.org 0x020E8F80 + 4 + 3*16 +  5
+	.db	0x01		// Room number
+// Disposal Center boss gate (might also be able to fix in the level blocks)
+//.org 0x020DC600 +   + 4*96 + 70
+//	.db	0x21		// World block
+//.org 0x020E0CD4 + 4 + 1*64 + 37
+//	.db	0x01		// Room number
+//.org 0x020E0CD4 + 4 + 1*64 + 38
+//	.db	0x02		// Room number
+//.org 0x020E0CD4 + 4 + 2*64 + 37
+//	.db	0x00		// Room number
+//.org 0x020E0CD4 + 4 + 2*64 + 38
+//	.db	0x02		// Room number
+//.org 0x020E0A4C + 4 + 2*64 + 37
+//	.db	0x03		// Room number
+//.org 0x020E0B90 + 4 + 1*64 + 38
+//	.db	0x02		// Room number
+//.org 0x020E0B90 + 4 + 2*64 + 37
+//	.db	0x00		// Room number
 
 
 
